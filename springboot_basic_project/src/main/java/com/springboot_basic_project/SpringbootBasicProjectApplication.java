f92122a45f749c30339f4210790bd8b689cdd0c9
Michele Gazzola <94122203+5526-michelegit@users.noreply.github.com>
2026-09-23 12:24:32 +0200

    created a basic maven project

---
+++ springboot_basic_project/src/main/java/com/springboot_basic_project/SpringbootBasicProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_basic_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBasicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBasicProjectApplication.class, args);
+	}
+
+}
