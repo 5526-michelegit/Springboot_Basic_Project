66bf45794142c1d780bf90e0a28e0358ebcefdd6
Michele Gazzola <94122203+5526-michelegit@users.noreply.github.com>
2026-09-24 21:50:20 +0200

    added data to H2 DB and properties for the execution of DB queries

---
+++ springboot_basic_project/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO prodotti (nome, descrizione, prezzo, quantita_disponibile, data_creazione, data_ultima_modifica)
+VALUES ('Tastiera meccanica', 'Tastiera RGB switch blu', 59.90, 25, NOW(), NOW());
+INSERT INTO prodotti (nome, descrizione, prezzo, quantita_disponibile, data_creazione, data_ultima_modifica)
+VALUES ('Mouse wireless', 'Mouse ergonomico', 24.50, 40, NOW(), NOW());
